c8e35fb726002882af6276a4e7773875aaeb26fe
rzwn28 <110657782+rzwn28@users.noreply.github.com>
2022-08-15 11:38:59 +0700

    Add files via upload

---
+++ TUGAS = belajar-select.sql
@@ -0,0 +1,80 @@
+UPDATE siswa SET
+temapatlahir = "INDRAMAYU"
+WHERE
+nis = "12002406";
+
+
+SELECT *FROM siswa 
+WHERE nis = "12002406";
+
+
+UPDATE siswa SET
+tanggallahir = "2005-05-05",
+alamat = "CIGADUNG"
+WHERE nis = "12002406";
+
+SELECT *FROM siswa 
+WHERE jk = "L";
+
+SELECT *FROM siswa
+WHERE jk = "L" AND temapatlahir = "BANDUNG";
+
+SELECT *FROM siswa
+WHERE jk="P" OR temapatlahir = "BANDUNG";
+
+SELECT *FROM siswa
+WHERE 
+tanggallahir BETWEEN "2004-10-01" AND "2004-12-30";
+
+SELECT *FROM siswa
+WHERE 
+nilai BETWEEN "80" AND "90";
+
+SELECT *FROM siswa
+WHERE nama LIKE "%O%";
+
+SELECT *FROM siswa
+WHERE nama LIKE "%U";
+
+SELECT *FROM siswa
+WHERE nama LIKE "A%";
+
+SELECT *FROM siswa
+WHERE nama LIKE "%ADI%";
+
+SELECT *FROM siswa
+ORDER BY nilai ASC
+
+SELECT *FROM siswa
+ORDER BY nilai DESC
+
+
+SELECT *FROM siswa
+WHERE
+nilai BETWEEN "80" AND "90"
+ORDER BY nilai ASC
+
+DELETE FROM siswa 
+WHERE nis = "12002406";
+
+SELECT tempatlahir
+FROM siswa
+GROUP BY tempatlahir;
+
+SELECT nama, kelas, nilai
+FROM siswa;
+
+SELECT *FROM siswa
+WHERE tempatlahir != "SUBANG";
+
+SELECT *FROM siswa
+WHERE nilai >=80 AND nilai <=90;
+
+CREATE DATABASE 12rpl1;
+
+
+
+
+
+
+
